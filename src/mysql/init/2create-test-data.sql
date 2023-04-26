1917997d9b80c5133cbc7eb16bdd3acaa29048da
Tore Hald Simonsen <42994990+tohasim@users.noreply.github.com>
2023-04-26 14:27:43 +0000

    Docker now runs.. Cannot connect to database

--- src/mysql/init/2create-test-data.sql
+++ src/mysql/init/2create-test-data.sql
@@ -1,3 +1,4 @@
+USE TEST;
 INSERT INTO TESTTABLE(message) VALUE ("Besked fra Database 1");
 INSERT INTO TESTTABLE(message) VALUE ("Besked fra Database 2");
 INSERT INTO TESTTABLE(message) VALUE ("Besked fra Database 3");
