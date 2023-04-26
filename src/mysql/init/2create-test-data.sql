2d7edb0c9a2bed7442b73ce9ec9477b2492d2da1
Tohasim <toreh.simonsen@live.dk>
2023-04-26 15:41:04 +0200

    Added SQL scripts

---
+++ src/mysql/init/2create-test-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TESTTABLE(message) VALUE ("Besked fra Database 1");
+INSERT INTO TESTTABLE(message) VALUE ("Besked fra Database 2");
+INSERT INTO TESTTABLE(message) VALUE ("Besked fra Database 3");
+commit;
